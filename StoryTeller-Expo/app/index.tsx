8835b863628a9d9b74419e57555e93a2e74a01ac
Tomas Bance <tomas.bance@gmail.com>
2024-05-30 14:47:26 +0200

    reset project, updated depen...

--- StoryTeller-Expo/app/index.tsx
+++ StoryTeller-Expo/app/index.tsx
@@ -1,97 +1,15 @@
-import { Image, StyleSheet, Platform, View, Text, Button } from 'react-native';
+import { Text, View } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { Link, router } from 'expo-router';
-
-async function fetchHello() {
-  const response = await fetch('/hello');
-  const data = await response.json();
-  alert('Hello ' + data.hello);
-}
-
-export default function HomeScreen() {
-
-  const route = ()=>{
-    return router.push('/settings');
-  }
-
-
+export default function Index() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <View className=" bg-slate-400" >
-          <Text className=" text-red-400">ttext</Text>  
-         </View>
-
-        <HelloWave />
-        <Button onPress={() => fetchHello()} title="Fetch hello" />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-
-
-
-      <Link href="/ChildScreen"> Navigate to ChildScreen</Link>
-      <Link href="/settings"> Navigate to settings</Link>
-      <Text onPress={route}>here is push</Text>
-
-
-
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
