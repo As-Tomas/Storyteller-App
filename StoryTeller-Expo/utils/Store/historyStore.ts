ae11e7e9fbb3dee9b050e4d8d10d156035077631
Tomas Bance <tomas.bance@gmail.com>
2024-06-11 17:27:41 +0200

    Refactor to use FlashList

--- StoryTeller-Expo/utils/Store/historyStore.ts
+++ StoryTeller-Expo/utils/Store/historyStore.ts
@@ -14,16 +14,22 @@ interface HistoryStore {
   addHistoryItem: (story: string, image: string, title: string) => void;
   removeHistoryItem: (index: number) => void;
   clearHistory: () => void;
+  // doubleHistory: () => void;
 }
+const MAX_HISTORY_SIZE = 200;
 
 export const useHistoryStore = create<HistoryStore>()(
   persist(
     (set) => ({
       history: [],
       addHistoryItem: (story, image, title) =>
-        set((state) => ({
-          history: [{ story, image, title, dateSaved: new Date() }, ...state.history],
-        })),
+        set((state) => {
+          const newHistory = [{ story, image, title, dateSaved: new Date() }, ...state.history];
+          if (newHistory.length > MAX_HISTORY_SIZE) {
+            newHistory.pop(); // Remove the oldest item if history exceeds the maximum size
+          }
+          return { history: newHistory };
+        }),
       removeHistoryItem: (index) =>
         set((state) => ({
           history: state.history.filter((_, i) => i !== index),
@@ -32,6 +38,10 @@ export const useHistoryStore = create<HistoryStore>()(
         set({
           history: [],
         }),
+        // doubleHistory: () =>          
+        //   set((state) => ({
+        //     history: [...state.history, ...state.history],
+        //   })),
     }),
     {
       name: 'history-store',
